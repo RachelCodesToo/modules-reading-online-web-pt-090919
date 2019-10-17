254f208637b7eab71ec3613317af1c0e04cd61a3
Anne Fausto <a.r.fausto@gmail.com>
2019-10-17 22:19:55 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,7 @@
-require_relative './lib/kid.rb'
-require_relative './lib/dancer.rb'
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
+
 class Dancer 
  extend FancyDance::ClassMethods
  include FancyDance::InstanceMethods
